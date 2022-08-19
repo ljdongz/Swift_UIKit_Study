93d5b6d4b05d4e6d2e500e2e715310943764f406
Jeong Dong <eejxdong99@gmail.com>
2022-08-19 20:51:54 +0900

    Timer-2

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    var timer = Timer()
+    let content = UNMutableNotificationContent()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
-
+    @IBAction func moveSlider(_ sender: UISlider) {
+        mainLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        mainLabel.text = String(Int(slider.value))
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        slider.value = 30
+        mainLabel.text = "초를 선택하세요"
+        content.sound = .defaultCritical
+    }
+    
+    func configureUI(){
+        slider.maximumValue = 60
+        slider.minimumValue = 0
+        slider.value = 30
+    }
+    
+    @objc func timerAction() {
+        if (slider.value - 1) < 0 {
+            timer.invalidate()
+            mainLabel.text = "종료"
+        }else{
+            slider.value -= 1
+            mainLabel.text = String(Int(slider.value))
+        }
+        
+    }
+    
+    
 }
 
